cd1e5bff5cd5341a4d0689a2dafa5f3718290c14
unknown <mgkim15@pearlabyss.com>
2026-07-10 14:45:41 +0900

    [Delete]: unused trash files and dir

--- trashes/prev/Materials.cpp
+++
@@ -1,6 +0,0 @@
-#include "Materials.h"
-
-void Materials::init(uint32_t material_count, uint32_t material_attribute_float4_count) {
-	material_count_ = material_count;
-	material_attribute_float4_count_ = material_attribute_float4_count;
-}
